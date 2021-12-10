3e1b7eef0d54bc5c9572096cb49bc98c1050484d
Anton Parkhimovich <pad1907@gmail.com>
2021-12-10 22:08:30 +0300

    fix:some styles

--- weatherappnextjs/components/WeatherCurrent.js
+++ weatherappnextjs/components/WeatherCurrent.js
@@ -9,6 +9,14 @@ export const WeatherCurrent = () => {
                 <DropDowm />
             </div>
             <div className={'forecast-hour__main'}>
+                 <ul className={'forecast-hour__header'}>
+                    <li className={'header-item'}>Time</li>
+                    <li className={'header-item'}>Temp</li>
+                    <li className={'header-item'}>Condition</li>
+                    <li className={'header-item'}>Wind speed</li>
+                    <li className={'header-item'}>Wind direction</li>
+                    <li className={'header-item'}>Pressure</li>
+                </ul>
                 {forecastDaySelect.hour.map((item) => {
                     return <ForecastHourItem item={item} key={item.time} />
                 })}
